b18c7ed43b500018039a0903c0742c571d7dc279
APACHE-0002 <48190578+APACHE-0002@users.noreply.github.com>
2021-08-28 20:15:09 -0300

    commitbase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import './styles.css'
 
-const UserContext = React.createContext({name: "Bruno Bertolini"});
-
-class UserProfile extends React.Component{
-  static contextType = UserContext;
-  render(){
-    return <div>{this.context.name}</div>
-  }
-}
+import Counter from "./components/Counter";
 
 export default function App() {
   return (
-    <UserContext.Provider value={{name: "arronja drado"}}>
-    <div className="App">
-      <UserProfile />
+    <div>
+        <Counter />
     </div>
-
-    </UserContext.Provider>     
 
   );
 }
