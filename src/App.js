d1a8884529444ac82de11c84122b92b56e7cd587
APACHE-0002 <48190578+APACHE-0002@users.noreply.github.com>
2021-09-09 12:55:04 -0300

    firstbase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import './styles.css'
 
-import Counter from "./components/Counter";
-import Mirror from "./components/Mirror";
-
-import CountProvider from "./context/Count";
 
 export default function App() {
-  //para habilitar a leitura e gravaçao de dados pelos componentes
-  //eles precisam ter um provider um nivel acima
-
-  //os componentes em um nivel inferior são o {children}
   return (
-    <CountProvider>
-        <Counter />
-
-        <hr/>
-
-        <Mirror /> 
-    </CountProvider>
+    <div>
+      <h1>churras</h1>
+    </div>
 
   );
 }
